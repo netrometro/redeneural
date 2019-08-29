4fe6835aae54c1260cfd42594d1a6a397580bd41
Ewerton Mendonca <netrometro@yahoo.com.br>
2019-08-28 21:35:29 -0300

    Implementação Neurônio

---
+++ src/br/Neuron.java
@@ -0,0 +1,57 @@
+package br;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Neuron {
+	
+	private long[] weights;
+	private long[] inputs;
+	private long[] results;
+	private long output;
+	
+	public Neuron(int amoutOfInputs) {
+		Random r = new Random();
+		weights = new long[amoutOfInputs];
+		for (int i = 0; i < weights.length; i++) {
+			weights[i] = r.nextInt(2001)-1000;
+		}
+	}
+
+	public Neuron(long[] weights) {
+		this.weights = weights.clone();
+	}
+	
+	public long interaction(long[] inputs) {
+		this.inputs = inputs.clone();
+		this.results = new long[inputs.length];
+		long output = 0;
+		for (int i = 0; i < weights.length; i++) {
+			results[i] = inputs[i] * weights[i];
+			output += results[i];
+		}
+		if (output < 0) 
+			output = 0;
+		return output;
+	}
+	
+	public long[] getDNA() {
+		return weights.clone();
+	}
+	
+	public void logWeights() {
+		System.out.println("Neuron [weights: " + Arrays.toString(weights) + " ]");
+	}
+	
+	public void logInputs() {
+		System.out.println("Neuron [inputs: " + Arrays.toString(inputs) + " ]");
+	}
+	
+	public void logResults() {
+		System.out.println("Neuron [results: " + Arrays.toString(results) + " ]");
+	}
+	
+	public void logOutput() {
+		System.out.println("Neuron [output: " + output + " ]");
+	}
+}
